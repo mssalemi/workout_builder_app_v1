ac591e51d0807d2e3911a799508fa49ff40065c7
Mehdi Salemi <mehdisalemi@Mehdis-MacBook-Air.local>
2024-03-04 21:22:04 -0500

    add find all exercises-hacky

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -20,5 +20,20 @@ module Types
 
       workout.graphql_data
     end
+
+    field :find_workouts_by_user, [Types::WorkoutType], null: false do
+      description "Find all workouts by User ID"
+      argument :user_id, GraphQL::Types::ID, required: true
+    end
+
+    def find_workouts_by_user(user_id:)
+      # Fetch the last 10 workouts by ID for the given user, assuming higher IDs are more recent.
+      workouts = Workout.where(user_id: user_id.to_i).order(id: :desc).limit(10)
+    
+      # Load and map each workout using your existing logic
+      workouts.map do |workout|
+        WorkoutBuilder::WorkoutBuilderWorkout.load_from_db(workout_id: workout.id)
+      end.map(&:graphql_data) # Ensure your Workout model or builder has a method `graphql_data` to format the data for GraphQL response
+    end
   end
 end
