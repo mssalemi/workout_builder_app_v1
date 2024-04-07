eb3d7725d9487cd0a89b64040c23121e986abff3
Mehdi Salemi <mehdisalemi@Mehdis-MacBook-Air.local>
2024-04-07 07:25:22 -0400

    great

--- app/models/week.rb
+++ app/models/week.rb
@@ -1,4 +1,4 @@
 class Week < ApplicationRecord
   belongs_to :workout_program
-  has_many :workouts, -> { order(order: :asc) }, dependent: :destroy
+  has_many :workouts, -> { order(order: :asc) }, foreign_key: 'workout_week_id', dependent: :destroy
 end
